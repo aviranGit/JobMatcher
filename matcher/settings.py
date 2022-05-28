feb32064515e9cc74befae22bdf20a2e18b3b2b4
aviranGit <avirangi@post.bgu.ac.il>
2022-05-28 18:22:49 +0300

    env + requrements.txt

--- matcher/settings.py
+++ matcher/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,9 +84,9 @@ WSGI_APPLICATION = 'matcher.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE'  : 'django.db.backends.mysql',  
-        'NAME'    : 'matcherdb',                  ## Change DB NAME 
-        'USER'    : 'root',                        ## Change USER 
-        'PASSWORD': 'admin',                        ## Change PASSWORD 
+        'NAME'    : env('DATABASE_NAME'),                  
+        'USER'    : env('DATABASE_USER'),                        
+        'PASSWORD': env('DATABASE_PASSWORD'), 
         'HOST'    : 'localhost',                
         'PORT'    : '3306',
     }
